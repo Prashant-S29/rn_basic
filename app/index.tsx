e591795c8324814739f4d8dcebba5f9649c51c78
prashant-s29 <prashant.s2922@gmail.com>
2025-04-30 11:45:40 +0530

    project setup

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native';
+
+import '@/styles/globals.css';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
